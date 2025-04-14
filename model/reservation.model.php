51280f10e23aefbe5a051567a4eebc383602e861
fredericsom2 <frederic.som@lapiscine.pro>
2025-04-14 15:20:38 +0200

    resa

--- model/reservation.model.php
+++ model/reservation.model.php
@@ -4,43 +4,49 @@ class Reservation {
 
 
 // création de plusieurs classe
-	public $name;
+        public $name;
 
-	public $place;
+        public $place;
 
-	public $startDate;
+        public $startDate;
 
-	public $endDate;
+        public $endDate;
 
-	public $totalPrice;
+        public $totalPrice;
 
-	public $nightPrice;
+        public $nightPrice;
 
-	public $status;
+        public $status;
 
-	public $bookedAt;
+        public $bookedAt;
 
-	public $cleaningOption;
-}
+        public $cleaningOption;
 
-// création d'un objet avec le mot clé "new" et stocké dans la class $reservation
-$reservation = new Reservation();
+        public function __construct() {
+        
 
 // je rempli chaque propriété via la variable 
 
-$reservation->name = "Fred Som";
-$reservation->place = "hotel bali";
-$reservation->startDate = new DateTime("25-04-15");
-$reservation->endDate = new DateTime("25-05-17");
-$reservation->cleaningOption = true;
-$reservation->nightPrice = 100;
+// "this" fait reference à la classe actuelle 
 
-// valeurs calculées automatiquement (calcul du prix total)
-$totalPrice = (($reservation->endDate->getTimestamp() - $reservation->startDate->getTimestamp()) / (3600 * 24) * $reservation->nightPrice) + 5000;
+            $this->name = "Fred Som";
+            $this->place = "hotel bali";
+            $this->startDate = new DateTime("25-04-15");
+            $this->endDate = new DateTime("25-05-17");
+            $this->cleaningOption = true;
+            $this->nightPrice = 100;
 
-$reservation->totalPrice = $totalPrice;
-$reservation->bookedAt = new DateTime();
-$reservation->status = "CART";
+            // valeurs calculées automatiquement (calcul du prix total)
+            $totalPrice = (($this->endDate->getTimestamp() - $this->startDate->getTimestamp()) / (3600 * 24) * $this->nightPrice) + 5000;
+
+            $this->totalPrice = $totalPrice;
+            $this->bookedAt = new DateTime();
+            $this->status = "CART";
+        }
+    }
+
+// création d'un objet avec le mot clé "new" et stocké dans la class $reservation
+$reservation = new Reservation();
 
 // utilisation de vardump pour vérifier 
 var_dump($reservation); 
